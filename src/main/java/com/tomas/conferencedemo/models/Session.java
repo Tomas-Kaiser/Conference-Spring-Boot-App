ce635ae3eedf1c3677a3ef2efad5d54b7f43fd3c
Tomas Kaiser <tomas.kaiser@optum.com>
2021-04-19 19:31:08 +0200

    add Session class & add connection DB

--- src/main/java/com/tomas/conferencedemo/models/Session.java
+++ src/main/java/com/tomas/conferencedemo/models/Session.java
@@ -1,4 +1,53 @@
 package com.tomas.conferencedemo.models;
 
+import javax.persistence.*;
+
+@Entity(name = "sessions") // The name matches with the table's name in DB
 public class Session {
+    @Column(name = "session_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sessionId;
+    @Column(name = "session_name")
+    private String sessionName;
+    @Column(name = "session_description")
+    private String sessionDescription;
+    @Column(name = "session_length")
+    private Integer sessionLength;
+
+    // Add empty constructor for serializing and deserializing data
+    public Session() {
+    }
+
+    public Long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(Long sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getSessionName() {
+        return sessionName;
+    }
+
+    public void setSessionName(String sessionName) {
+        this.sessionName = sessionName;
+    }
+
+    public String getSessionDescription() {
+        return sessionDescription;
+    }
+
+    public void setSessionDescription(String sessionDescription) {
+        this.sessionDescription = sessionDescription;
+    }
+
+    public Integer getSessionLength() {
+        return sessionLength;
+    }
+
+    public void setSessionLength(Integer sessionLength) {
+        this.sessionLength = sessionLength;
+    }
 }
