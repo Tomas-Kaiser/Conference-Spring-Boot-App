19367124be62f8c38ae2cb71abbe757edab6d312
Tomas Kaiser <tomas.kaiser@optum.com>
2021-04-19 22:09:36 +0200

    Add controllers

---
+++ src/main/java/com/tomas/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,48 @@
+package com.tomas.conferencedemo.controllers;
+
+import com.tomas.conferencedemo.models.Session;
+import com.tomas.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/sessions")
+public class SessionsController {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> getAllSessions() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Session getSession(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session createSession(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delesteSession(Long id) {
+        // Also need to check for children records before deleting!
+        sessionRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Session updateSession(@PathVariable Long id, @RequestBody final Session session) {
+        // TODO: Check if all properties are field otherwise it will be a null or use patch instead
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "sessionId");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
+}
---
+++ src/main/java/com/tomas/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,47 @@
+package com.tomas.conferencedemo.controllers;
+
+import com.tomas.conferencedemo.models.Speaker;
+import com.tomas.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/speakers")
+public class SpeakersController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> getAllSpeakers() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Speaker getSpeaker(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker createSpeaker(@RequestBody final Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSpeaker(@PathVariable Long id) {
+        // Also need to check for children records before deleting!
+        speakerRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Speaker updateSpeaker(@PathVariable Long id, @RequestBody final Speaker speaker) {
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speakerId");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
